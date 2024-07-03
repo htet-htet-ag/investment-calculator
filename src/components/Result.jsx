64c03f7c43391b6dcc4f5cfec295d716582e9080
Htet Htet Aung <htethtetaung@GKMacBookPro.local>
2024-07-03 15:47:50 +0630

    investment calculator by react

---
+++ src/components/Result.jsx
@@ -0,0 +1,40 @@
+import { calculateInvestmentResults } from "../util/investment";
+import { formatter } from "../util/investment";
+export default function Result({ input }) {
+  const resultData = calculateInvestmentResults(input);
+  const initialInvestment =
+    resultData[0].valueEndOfYear -
+    resultData[0].interest -
+    resultData[0].annualInvestment;
+  return (
+    <table id="result">
+      <thread>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Captial</th>
+        </tr>
+        <tbody>
+          {resultData.map((yearData) => {
+            const totalInterest =
+              yearData.valueEndOfYear -
+              yearData.annualInvestment * yearData.year -
+              initialInvestment;
+            const totalAmountInterest = yearData.valueEndOfYear - totalInterest;
+            return (
+              <tr key={yearData.year}>
+                <td>{yearData.year}</td>
+                <td>{formatter.format(yearData.valueEndOfYear)}</td>
+                <td>{formatter.format(yearData.interest)}</td>
+                <td>{formatter.format(totalInterest)}</td>
+                <td>{formatter.format(totalAmountInterest)}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </thread>
+    </table>
+  );
+}
